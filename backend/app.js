cac268c83dc9041e7ba103ad63351196600c49ec
corentinventejol <97588972+corentinventejol@users.noreply.github.com>
2023-08-29 16:12:05 +0200

    affichage creation et modification des sauces
    j'ai ajoutez la possibilité d'ajouter de modifier et d'afficher les sauces que je stock sur mangoDB

--- backend/app.js
+++ backend/app.js
@@ -8,18 +8,11 @@ const userRoutes = require('./routes/user');
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  console.log(req.headers);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", 
-             "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-});
-
-app.get('http://localhost:3000/', function(req, res){
-  var data = {'message': 'Server is running'}
-  res.jsonp(data);
 });
 
 mongoose.connect('mongodb+srv://corentin:corentin@cluster0.rhk6ig7.mongodb.net/?retryWrites=true&w=majority',
@@ -28,7 +21,7 @@ mongoose.connect('mongodb+srv://corentin:corentin@cluster0.rhk6ig7.mongodb.net/?
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  app.use('/api/stuff', stuffRoutes);
+  app.use('/api/sauces', stuffRoutes);
   app.use('/api/auth', userRoutes);
   app.use('/images', express.static(path.join(__dirname, 'images')));
 
--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -50,24 +50,20 @@ exports.updateSauce = (req, res, next) => {
     });
 };
 
+//revoir le deleteSauce pas fonctionnel 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
-      if (sauce.userId !== req.auth.userId) {
-        return res.status(401).json({ message: 'Non autorisé' });
-      }
-
-      const filename = sauce.imageUrl.split('/images/')[1];
+      const filename = sauce.imageUrl.split("/images/")[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
           .catch((error) => res.status(400).json({ error }));
       });
     })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+    .catch((error) => res.status(500).json({ error }));
 };
+
 
 exports.getAllSauces = (req, res, next) => {
   Sauce.find()
